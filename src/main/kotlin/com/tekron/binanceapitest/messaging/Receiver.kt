dcaa96522ce82e48e45a04a71c13ddd7e4ce47af
Hakan Nizamoglu <hnizamoglu@gmail.com>
2024-08-24 15:03:39 +0200

    test

--- src/main/kotlin/com/tekron/binanceapitest/messaging/Receiver.kt
+++ src/main/kotlin/com/tekron/binanceapitest/messaging/Receiver.kt
@@ -3,6 +3,7 @@ package com.tekron.binanceapitest.messaging
 import com.tekron.binanceapitest.BinanceKlineProcessor
 import com.tekron.binanceapitest.adapter.CoinSymbol
 import io.github.oshai.kotlinlogging.KotlinLogging
+import io.sentry.Sentry
 import org.springframework.amqp.rabbit.annotation.RabbitHandler
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
@@ -16,6 +17,7 @@ class Receiver(
 
     @RabbitHandler
     fun receive(message: String) {
+        Sentry.captureMessage("Processing $message")
         processor.process(CoinSymbol.valueOf(message))
     }
 }
